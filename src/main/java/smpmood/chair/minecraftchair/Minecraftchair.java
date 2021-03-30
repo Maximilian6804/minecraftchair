68e67519520c2e76769b8f0cd7b7c651953b988a
BuildTools <unconfigured@null.spigotmc.org>
2021-03-30 02:08:12 +0200

    I like dogs

--- src/main/java/smpmood/chair/minecraftchair/Minecraftchair.java
+++ src/main/java/smpmood/chair/minecraftchair/Minecraftchair.java
@@ -24,7 +24,7 @@ public final class Minecraftchair extends JavaPlugin implements Listener {
             Material.SANDSTONE_STAIRS, Material.SMOOTH_SANDSTONE_STAIRS, Material.SPRUCE_STAIRS, Material.OAK_STAIRS, Material.BLACKSTONE_STAIRS, Material.POLISHED_BLACKSTONE_BRICK_STAIRS, Material.POLISHED_BLACKSTONE_STAIRS,
             Material.DARK_PRISMARINE_STAIRS, Material.PRISMARINE_BRICK_STAIRS, Material.PRISMARINE_STAIRS, Material.SMOOTH_RED_SANDSTONE_STAIRS, Material.CRIMSON_STAIRS, Material.POLISHED_ANDESITE_STAIRS, Material.POLISHED_DIORITE_STAIRS,
             Material.ANDESITE_STAIRS, Material.DIORITE_STAIRS, Material.GRANITE_STAIRS, Material.STONE_STAIRS, Material.STONE_BRICK_STAIRS, Material.MOSSY_COBBLESTONE_STAIRS, Material.MOSSY_STONE_BRICK_STAIRS, Material.RED_NETHER_BRICK_STAIRS,
-            Material.WARPED_STAIRS, Material.END_STONE_BRICK_STAIRS});
+            Material.WARPED_STAIRS, Material.END_STONE_BRICK_STAIRS, Material.POLISHED_GRANITE_STAIRS});
 
     public Minecraftchair() {Instance = this;}
 
@@ -51,7 +51,7 @@ public final class Minecraftchair extends JavaPlugin implements Listener {
         Player p = event.getPlayer();
         if (event.getAction().equals(Action.RIGHT_CLICK_BLOCK)) {
             if (halfBlockTypes.contains(event.getClickedBlock().getType())){
-                if (!(event.getPlayer().getInventory().getItemInMainHand().getType().isBlock()) || (event.getPlayer().getInventory().getItemInMainHand().getType() == Material.AIR)) {
+                if (!((event.getPlayer().getInventory().getItemInMainHand().getType().isBlock() && (event.getPlayer().getInventory().getItemInOffHand().getType().isBlock()) && (event.getPlayer().getInventory().getItemInMainHand().getType() == Material.LAVA_BUCKET) && (event.getPlayer().getInventory().getItemInMainHand().getType() == Material.WATER_BUCKET))) || (event.getPlayer().getInventory().getItemInMainHand().getType() == Material.AIR)) {
                 Location loc = event.getClickedBlock().getLocation();
                 World world = event.getPlayer().getWorld();
                 Location loc2 = new Location(world, loc.getX() + 0.5, loc.getY() - 1.15, loc.getZ() + 0.5);
